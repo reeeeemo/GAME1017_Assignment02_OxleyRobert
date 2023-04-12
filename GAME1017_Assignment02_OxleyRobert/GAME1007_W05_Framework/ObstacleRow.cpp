897cc257d3def19d48cc99dce6de09b36b118d1b
reeeeemo <robbie@oxley.ca>
2023-04-11 22:37:53 -0400

    [AIDEN]

--- GAME1017_Assignment02_OxleyRobert/GAME1007_W05_Framework/ObstacleRow.cpp
+++ GAME1017_Assignment02_OxleyRobert/GAME1007_W05_Framework/ObstacleRow.cpp
@@ -15,7 +15,7 @@ ObstacleRow::ObstacleRow():m_gapCtr(0), m_gapMax(3)
 
 	// Load the textures
 	TEMA::Load("../Assets/img/obstacles/Tilesets/trees.png", "obstacleSheet");
-	TEMA::Load("../Assets/img/obstacles/Tilesets/god.png", "god");
+	TEMA::Load("../Assets/img/obstacles/Tilesets/god1.png", "god");
 
 }
 
@@ -59,7 +59,7 @@ void ObstacleRow::Update()
 				}
 				else
 				{
-					auto obst = new Obstacle({ 0 + 128.0f*10, 384.0f, 128.0f, 168.0f }, true, new Image({ 0, 0, 1280, 720 }, { 0, 184, 110, 184 }, "god"));
+					auto obst = new Obstacle({ 0 + 128.0f*10, 384.0f, 110.0f, 184.0f }, true, new Image({ 0, 0, 1280, 720 }, { 0, -184, 110, 184 }, "god"));
 					obst->SetIsHazard(false);
 					m_obstacles.push_back(obst);
 				}
